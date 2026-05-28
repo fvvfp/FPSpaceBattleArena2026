0f7a8e064aa01fed1592b937c02c57e42f5e6c72
Finian Pasterczyk <finian.pasterczyk@gmail.com>
2026-05-28 09:50:29 -0700

    middle ship 2 v1

--- MiddleShip2.java
+++ MiddleShip2.java
@@ -13,7 +13,7 @@ public class MiddleShip2 extends BasicSpaceship {
    private double maxSpeed;
    private boolean startup = true;
    private ArrayList<ShipCommand> shipQueue;
-   
+   private boolean justDidRadar = false;
    private boolean healing = false;
 
     public static void main(String[] args)
@@ -41,68 +41,109 @@ public class MiddleShip2 extends BasicSpaceship {
       }
 
       ObjectStatus shipStatus = env.getShipStatus();
+      
       if (startup) {
          shipMass = shipStatus.getMass();
          maxSpeed = shipStatus.getMaxSpeed();
          startup = false;
       }
-
       
-      if (!healing && shipStatus.getHealth() < shipStatus.getMaxHealth()) {
-         healing = true;
-         return new RepairCommand((int)(shipStatus.getMaxHealth() - shipStatus.getHealth()));
+      if (justDidRadar) {
+         justDidRadar = false;
+         return doTorpedo(shipStatus, env.getRadar());
+      }
+
+      if (shipStatus.getSpeed() > 1.0) {
+         return new BrakeCommand(0.0);
+      }
+      
+      if (shipStatus.getEnergy() > 30.0) {
+         justDidRadar = true;
+         return new RadarCommand(5);
       }
 
       double distanceToCenter = shipStatus.getPosition().getDistanceTo(center);
-      if (distanceToCenter < 170) {
-         if (shipStatus.getSpeed() > 1.0) {
-            return new BrakeCommand(0.0);
-         }
+      if (distanceToCenter < 175) {
          return new IdleCommand(0.1);
       }
-
-      healing = false;
       
-      Point desiredVel = new Point(center.getX()-shipStatus.getPosition().getX(), center.getY()-shipStatus.getPosition().getY());
-      Point currentVelVec = new Point(
-         shipStatus.getSpeed()*Math.cos(shipStatus.getOrientation()*Math.PI/180.0),
-         shipStatus.getSpeed()*Math.sin(shipStatus.getOrientation()*Math.PI/180.0)
-      );
-      Point desiredThrustVec = new Point(
-         desiredVel.getX() - currentVelVec.getX(),
-         desiredVel.getY() - currentVelVec.getY()
-      );
-      int thrustAngle = (int)( (Math.atan2((double)desiredThrustVec.getY(), (double)desiredThrustVec.getX()) + Math.PI/2.0) * 180/Math.PI );
-      System.out.println(thrustAngle);
-      
-      int shipAngle = shipStatus.getOrientation();
-      if (thrustAngle - shipAngle < 0) thrustAngle += 360;
-      int relativeAngle = thrustAngle - shipAngle;
-      int dirCode = (relativeAngle + 45) / 90;
-      char towardCenterChar;
-      switch (dirCode) {
-         case 0:
-            towardCenterChar = 'B';
-            break;
-         case 1:
-            towardCenterChar = 'R';
-            break;
-         case 2:
-            towardCenterChar = 'F';
-            break;
-         case 3:
-            towardCenterChar = 'L';
-            break;
-         default:
-            towardCenterChar = 'B';
-            break;
-      }
-      shipQueue.add(new RotateCommand(relativeAngle - 90 * dirCode));
-
-      //shipQueue.add(new ThrustCommand(towardCenterChar, 1.0, 1.0, true));
+      doMovement(shipStatus, distanceToCenter);
+      shipQueue.add(new BrakeCommand(0.0));
 
       //return first in queue
       return shipQueue.remove(0);
       
     }
+    
+    public ShipCommand doTorpedo(ObjectStatus shipStatus, ArrayList<ObjectStatus> radarResults) {
+      ObjectStatus target = null;
+      double score = 999999.09;
+      for (ObjectStatus object : radarResults) {
+         if (object.getType().equals("Ship") || object.getType().equals("Asteroid")) {
+            double oScore = object.getSpeed();
+            if (oScore < score) {
+               score = oScore;
+               target = object;
+            }
+         }
+      }
+      
+      if (target == null) return new IdleCommand(0.1);
+      
+      shipQueue.add(new FireTorpedoCommand('F'));
+      return new RotateCommand((shipStatus.getPosition().getAngleTo(target.getPosition())) - shipStatus.getOrientation());
+    }
+    
+    public void doMovement(ObjectStatus shipStatus, double distanceToCenter) {
+      int angleToCenter = shipStatus.getPosition().getAngleTo(center);
+      int shipAngle = shipStatus.getOrientation();
+      if (angleToCenter - shipAngle < 0) angleToCenter += 360;
+      int relativeAngle = angleToCenter - shipAngle;
+      int dirCode = (relativeAngle + 45) / 90;
+      char towardCenterChar;
+      char awayFromCenterChar;
+      switch (dirCode) {
+         case 0:
+            towardCenterChar = 'B';
+            awayFromCenterChar = 'F';
+            break;
+         case 1:
+            towardCenterChar = 'R';
+            awayFromCenterChar = 'L';
+            break;
+         case 2:
+            towardCenterChar = 'F';
+            awayFromCenterChar = 'B';
+            break;
+         case 3:
+            towardCenterChar = 'L';
+            awayFromCenterChar = 'R';
+            break;
+         default:
+            towardCenterChar = 'B';
+            awayFromCenterChar = 'F';
+            break;
+      }
+      shipQueue.add(new RotateCommand(relativeAngle - 90 * dirCode));
+
+      double timeToFullSpeed = maxSpeed * shipMass / thrustPower;
+      double timeToHalfDistance = Math.sqrt( (double)(distanceToCenter-145)*shipMass/thrustPower );
+      if (timeToFullSpeed > timeToHalfDistance) {
+         shipQueue.add(new ThrustCommand(towardCenterChar, timeToHalfDistance, 1.0, true));
+         shipQueue.add(new ThrustCommand(awayFromCenterChar, timeToHalfDistance, 1.0, true));
+      } else {
+         double distanceInTimeToFullSpeed = timeToFullSpeed*timeToFullSpeed*3500/2/shipMass;
+         double distanceIdling = distanceToCenter - 2 * distanceInTimeToFullSpeed;
+         double timeIdling = distanceIdling / maxSpeed;
+         if (timeIdling >= 0.1) {
+            shipQueue.add(new ThrustCommand(towardCenterChar, timeToFullSpeed, 1.0, true));
+            shipQueue.add(new IdleCommand(timeIdling));
+            shipQueue.add(new ThrustCommand(awayFromCenterChar, timeToFullSpeed, 1.0, true));
+         } else {
+            shipQueue.add(new ThrustCommand(towardCenterChar, timeToFullSpeed + timeIdling, 1.0, true));
+            shipQueue.add(new ThrustCommand(awayFromCenterChar, timeToFullSpeed, 1.0, true));
+         }
+      }
+    }
+    
 }
